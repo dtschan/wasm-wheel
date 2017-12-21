757909d43cdebb114f299aba57747d747fc7292f
Boyan Mihaylov <hey@boyan.io>
2017-12-21 16:55:39 +0100

    Update rand create to 0.4 and use the main version rather than the fork

--- src/langs/rust/build.js
+++ src/langs/rust/build.js
@@ -1,17 +1,17 @@
-const { exec } = require('child_process');
 const fs = require('fs');
+const { exec } = require('child_process');
 
 exports.task = (done) => {
     const buildDir = `${__dirname}/../../../build/wasm`;
 
-    exec('cargo  +nightly build --target wasm32-unknown-unknown --release', { cwd: __dirname })
-        .then(({ stdout }) => {
-            console.log(stdout);
+    const ls = exec('cargo +nightly build --target wasm32-unknown-unknown --release', { cwd: __dirname });
+    ls.stdout.pipe(process.stdout)
+    ls.stderr.pipe(process.stdout)
+    ls.on('exit', (code) => {
+        if (code !== 0)
+            throw Error('Error when building the Rust wheel part');
 
-            fs.copyFileSync(`${__dirname}/target/wasm32-unknown-unknown/release/wasm_wheel.wasm`, `${buildDir}/wheel-part-rust.wasm`);
-            done();
-        }, ({ stderr, cmd }) => {
-            console.log(stderr);
-            throw Error(`Error when running: ${cmd}`);
-        });
+        fs.copyFileSync(`${__dirname}/target/wasm32-unknown-unknown/release/wasm_wheel.wasm`, `${buildDir}/wheel-part-rust.wasm`);
+        done();
+    });
 };
